10f5e1145250586b61a6da03216ce81e788a4e93
alberry <altrino@uw.edu>
2020-03-11 22:06:26 -0700

    DONE

--- index.js
+++ index.js
@@ -1,57 +1,187 @@
-// append svg to body
-let svg = d3.select('body').append('svg')
+let data = []
+
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 80, left: 80}
+    , width = 960 - margin.left - margin.right
+    , height = 500 - margin.top - margin.bottom;
+
+
+// set the ranges
+var x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.1);
+var y = d3.scaleLinear()
+            .range([height, 0]);
+        
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", 
+            "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("text")
+    .attr("text-anchor", "middle")  
+    .attr("transform", "translate("+ -40 + "," + 230 + ")rotate(-90)") 
+    .text("Counts")
+
+svg.append("text")
+    .attr("text-anchor", "middle") 
+    .attr("transform", "translate(" + 430 + "," + 450 + ")")
+    .text("Symptom")
+
+    // append the div which will be the tooltip
+    // append tooltipSvg to this div
+var div = d3.select('body').append('div')
+    .attr('class', 'tooltip')
     .attr('width', 500)
     .attr('height', 500)
-    .attr('id', 'svg')
+    .style('opacity', 0)
+    .style("position", "absolute")
+    .style("background-color", "rgba(255, 255, 255, 0.8)")
+    .style("border-radius", "5px")
+
+let tooltipSvg = div.append('svg')
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr('id', 'tooltip')
+
+tooltipSvg.append("text")
+    .attr("text-anchor", "middle")  
+    .attr("transform", "translate("+ 18 + "," + 240 + ")rotate(-90)") 
+    .text("Counts")
+
+tooltipSvg.append("text")
+    .attr("text-anchor", "middle") 
+    .attr("transform", "translate(" + 252 + "," + 485 + ")")
+    .text("Severity")
+    
 
 // make x scaling function and append x-axis
 let xScale = d3.scaleLinear()
-    .domain([0, 100])
+    .domain([0, 6])
     .range([50, 450])
 
 let xAxis = d3.axisBottom(xScale)
 
-svg.append('g')
+tooltipSvg.append('g')
     .attr('id', 'x-axis')
     .attr('transform', 'translate(0,450)')
     .call(xAxis)
 
-// set the parameters for the histogram
-var histogram = d3.histogram()
-    .value(function(d) { return d.symptom; })
-    .domain(xScale.domain())
-    //.thresholds(x.ticks(d3.timeMonth));
+d3.csv("SYMPTOM_MOCK_DATA.csv")
+    .then((csvData) => data = csvData)
+    .then(() => makeBarGraph())
 
-/******************************************
- * HISTOGRAM SECTION
- ******************************************/
-d3.csv("SYMPTOM_MOCK_DATA.csv", function(error, data) {
-    if (error) throw error;
 
-    // format the data
+function createHist(symptom, csv) {
+    tooltipSvg.selectAll("#y-axis").remove()
+    tooltipSvg.selectAll("rect").remove()
+    div.style('opacity', 1)
+
+    let filtered = csv.filter(d => d.symptom == symptom);
+    let severity = []
+    for (var i = 0; i < filtered.length; i++) {
+        severity.push(+filtered[i].severity)
+    }
+    
+
+    // set parameters for our histogram function
+    let histogram = d3.histogram()
+        .value(function(d) { return d }) 
+        .domain([0, 6])
+        .thresholds(xScale.ticks(10)) 
+
+    // get our bins
+    let bins = histogram(severity)
+
+    let max = 0;
+    for (let i = 0; i < bins.length; i++) {
+        if (bins[i].length > max) {
+            max = bins[i].length
+        }
+    }
+
+    let yScale = d3.scaleLinear()
+        .domain([max, 0])
+        .range([50, 450])
+
+    let yAxis = d3.axisLeft(yScale)
+
+    tooltipSvg.append('g')
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(50,0)')
+        .call(yAxis)
+
+    // append bars of histogram
+    tooltipSvg.selectAll('.rect')
+        .data(bins) // use the bins data 
+        .enter()
+        .append('rect')
+            .attr('x', function(d) { return xScale(d.x0) - 15}) 
+            .attr('y', function(d) { return yScale(d.length) - 0.55})
+            .attr('width', function(d) { return xScale(d.x1) - xScale(d.x0) })
+            .attr('height', function(d) { return 450 - yScale(d.length) })
+            .attr('fill', 'steelblue')
+            .attr('stroke', 'steelblue')
+}
+
+function makeBarGraph() {
+    // define count object that holds count for each city
+    var countObj = {};
+
+    // count how much each city occurs in list and store in countObj
     data.forEach(function(d) {
-        d.severity = +d.severity;
+        var symptom = d.symptom;
+        if(countObj[symptom] === undefined) {
+            countObj[symptom] = 0;
+        } else {
+            countObj[symptom] = countObj[symptom] + 1;
+        }
     });
 
-    // group the data for the bars
-    var bins = histogram(data);
+    var newData = new Object()
+    newData.symptom = Object.keys(countObj)
+    newData.counts = Object.values(countObj)
 
-    // Scale the range of the data in the y domain
-    yScale.domain([0, d3.max(bins, function(d) { return d.length; })]);
+    console.log(newData)
 
-    // append the bar rectangles to the svg element
-    svg.selectAll("rect")
-        .data(bins)
-    .enter().append("rect")
+    // Scale the range of the data in the domains
+    x.domain(data.map(function(d) { return d.symptom; }));
+    y.domain([0, d3.max(newData.counts)]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(newData.counts)
+        .enter()
+        .append("rect")
+        .attr("fill", "#F27E25")
         .attr("class", "bar")
-        .attr("x", 1)
-        .attr("transform", function(d) {
-            return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); });
+        .attr("x", function(d, i) { return x(newData.symptom[i]); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d); })
+        .attr("height", function(d) { return height - y(d); })
+        .on("mouseover", function(d, i) {
+            createHist(newData.symptom[i], data)
+        })
+        .on("mousemove", function() {
+            div.style("top", (event.pageY - 50) + "px")
+                .style("left",(event.pageX + 10) + "px")
+        })
+        .on("mouseout", function(d) {
+            div.style('opacity', 0)
+        })
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
     // add the y Axis
     svg.append("g")
-        .call(d3.axisLeft(yScale));
-        
-});
+        .call(d3.axisLeft(y));
+}
+
